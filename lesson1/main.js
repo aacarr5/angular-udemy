e54ce939b0798bd1412c503f519e9ed69aa914bb
Andrew Carr <aacarr5@gmail.com>
2016-02-10 14:27:31 -0600

    finish section 2

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,15 +1,23 @@
-var app=angular.module('minmax',[])
+var app=angular.module('minmax',['jcs-autoValidate'])
 
 app.controller("MinMaxCtrl",function($scope,$http) {
   $scope.formModel = {};
 
-  $scope.onSubmit = function(){
-  	$http.post('https://minmax-server.herokuapp.com/register/',$scope.formModel).
-  	success(function(data){
-  		console.log(":)")
-  	}).
-  	error(function(error){
-  		console.log(error)
-  	})
+  $scope.onSubmit = function(valid){
+    if (valid) {
+      console.log("Hey! I'm valid!");
+      console.log($scope.formModel)
+    } else {
+      console.log("Womp Womp")
+    }
+
+  	// $http.post('https://minmax-server.herokuapp.com/register/',$scope.formModel).
+  	// success(function(data){
+  	// 	console.log(":)");
+    //   console.log($scope.formModel);
+  	// }).
+  	// error(function(error){
+  	// 	console.log(error)
+  	// })
   }
 });
